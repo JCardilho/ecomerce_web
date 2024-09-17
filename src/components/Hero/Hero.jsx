f38dee87d14fdd1fddcec6734335ca1707930985
JCardilho <jose.1433@hotmail>
2024-09-17 19:11:02 -0300

    image section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Image1 from "../../assets/hero/headphone.png";
+import Image1 from "../../assets/women/women.png";
 import Image2 from "../../assets/hero/watch.png";
 import Image3 from "../../assets/hero/headphone.png";
 
@@ -28,13 +28,47 @@ const ImageList = [
 ];
 
 const Hero = () => {
-  return <div>
-    {/*bacground pattern*/}
-    <div className="h-[700px] w-[700px] bg-primary/40 ">
-        
+  return (
+    <div
+      className="relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center
+  items-center dark:bg-gray-950 dark:text-white duration-200 "
+    >
+      {/*bacground pattern*/}
+      <div className="h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3x1 rotate-45 -z-9 "></div>
+      {/*hero section */}
+      <div className="container pb-8 sm:pb-0">
+        <div>
+          <div className="grid grid-cols-1 sm:grid-cols-2">
+            {/*text content section */}
+            <div 
+            className="flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left
+            order-2 sm:order-1 relative z-10">
+              <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold">Lorem ipsum dolor sit   1:06 no video</h1>
+              <p className="text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.Vitae,
+                ad?  
+              </p>
+              <div>
+                <button className="bg-gradient-to-r
+                 from-primary to-secondary hover:scale-105 
+                 duration-200 text-white py-2 px-4 rounded-full">
+                    Order Now</button>
+              </div>
+            </div>
+
+            {/*image section */}
+            <div className="order-1 sm:order-2">
+                <div className="relative z-10">
+                    <img src={Image1} alt=""
+                    className="w-[300px] h-[300px] sm:h-[450] sm:w-[450] sm:scale-125 object-contain mx-auto"
+                    />
+                </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    {/*hero section */}
-  </div>;
+  );
 };
 
 export default Hero;
